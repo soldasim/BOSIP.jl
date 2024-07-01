8b329d4202acc8eaade61de7dc42d76ad4d6faae
Sheld5 <simonsoldat@seznam.cz>
2024-07-01 12:02:08 +0200

    update 'examples/diagnostic_selection'

--- examples/diagnostic_selection/plot.jl
+++ examples/diagnostic_selection/plot.jl
@@ -6,6 +6,10 @@ using BOSS, BOLFI
 using Distributions
 include("toy_problem.jl")
 
+# Select between plotting the true posterior
+# or the expected posterior in the first subplot.
+#
+const TRUE_POST = true
 
 function init_plotting(; save_plots, plot_dir)
     if save_plots
@@ -26,6 +30,8 @@ mutable struct PlotCallback <: BolfiCallback
     save_plots::Bool
     plot_dir::String
     put_in_scale::Bool
+    square_layout::Bool
+    ftype::String
 end
 PlotCallback(;
     plot_each = 1,
@@ -33,13 +39,15 @@ PlotCallback(;
     save_plots,
     plot_dir = ".",
     put_in_scale,
-) = PlotCallback(0, plot_each, term_cond, save_plots, plot_dir, put_in_scale)
+    square_layout = true,
+    ftype = "png",
+) = PlotCallback(0, plot_each, term_cond, save_plots, plot_dir, put_in_scale, square_layout, ftype)
 
 function (plt::PlotCallback)(problem::BolfiProblem; acquisition, options, kwargs...)
     if plt.iters % plt.plot_each == 0
         options.info && @info "Plotting ..."
         title = "Iteration $(plt.iters)"
-        plot_state(problem; term_cond=plt.term_cond, iter=plt.iters, save_plots=plt.save_plots, plot_dir=plt.plot_dir, plot_name="p_$(plt.iters)", put_in_scale=plt.put_in_scale, acquisition=acquisition.acq, title, separate=true)
+        plot_state(problem; ftype=plt.ftype, square_layout=plt.square_layout, term_cond=plt.term_cond, iter=plt.iters, save_plots=plt.save_plots, plot_dir=plt.plot_dir, plot_name="p_$(plt.iters)", put_in_scale=plt.put_in_scale, acquisition=acquisition.acq, title, separate=true)
     end
     plt.iters += 1
 end
@@ -101,15 +109,23 @@ const set_titles = [
 
 # axis label annotation offsets
 # (`+` is always outwards)
-LO(::Val{:T1}) = [
+LO(::Val{:T1Column}) = [
     -0.3 +0.5  # xlabel
     +0.6 -0.2  # ylabel
 ]
-LO(::Val{:T2}) = [
+LO(::Val{:T1Square}) = [
+    -0.3 +0.4  # xlabel
+    +0.7 -0.2  # ylabel
+]
+LO(::Val{:T2Column}) = [
     -0.3 +0.5  # xlabel
     +0.7 -0.2  # ylabel
 ]
-LO(::Val{:TruePost}) = [
+LO(::Val{:T2Square}) = [
+    -0.3 +0.4  # xlabel
+    +0.6 -0.2  # ylabel
+]
+LO(::Val{:SinglePlot}) = [
     -0.3 +0.4  # xlabel
     +0.6 -0.2  # ylabel
 ]
@@ -125,7 +141,7 @@ function separate_new_datum(problem)
     return bolfi, new
 end
 
-function plot_state(problem; term_cond, iter, display=true, save_plots=false, plot_dir=".", plot_name="p", put_in_scale=false, acquisition, title=nothing, separate=false)
+function plot_state(problem; ftype="png", square_layout=true, term_cond, iter, display=true, save_plots=false, plot_dir=".", plot_name="p", put_in_scale=false, acquisition, title=nothing, separate=false)
     if separate
         bolfi, new_datum = separate_new_datum(problem)
     else
@@ -134,55 +150,72 @@ function plot_state(problem; term_cond, iter, display=true, save_plots=false, pl
     step = 0.05 # TODO 0.05
     
     if problem isa BolfiProblem{Nothing}
-        p = plot_sbi(bolfi; new_datum, iter, term_cond, put_in_scale, acquisition, step)
+        p = plot_sbi(bolfi; square_layout, new_datum, iter, term_cond, put_in_scale, acquisition, step)
     else
-        p = plot_sbfs(bolfi; new_datum, iter, term_cond, put_in_scale, acquisition, step)
+        p = plot_sbfs(bolfi; square_layout, new_datum, iter, term_cond, put_in_scale, acquisition, step)
     end
     
     plot!(p; dpi=100)  # (the plot is huge)
     display && Plots.display(p)
-    save_plots && savefig(p, plot_dir * '/' * plot_name * ".pdf")
+    save_plots && savefig(p, plot_dir * '/' * plot_name * '.' * ftype)
 end
 
-function plot_sbi(bolfi; new_datum=nothing, iter=nothing, term_cond, put_in_scale=false, acquisition, step=0.05)
-    @assert acquisition isa PostVariance
-    mode = :T1
-    noise_vars_true = ToyProblem.σe_true(Val(mode)) .^ 2
-    y_dim = ToyProblem.y_dim(Val(mode))
+function plot_sbi(bolfi; square_layout=true, new_datum=nothing, iter=nothing, term_cond, put_in_scale=false, acquisition, step=0.05)
+    # @assert acquisition isa PostVariance  # TODO
+    mode = square_layout ? :T1Square : :T1Column
+    noise_vars_true = ToyProblem.σe_true() .^ 2
+    y_dim = ToyProblem.y_dim()
 
     title = "ITERATION $iter"
-    prefixes = ["(a$iter) ", "(b$iter) ", "(c$iter) ", "(d$iter) "]
+    prefixes = square_layout ?
+        ["(a) ", "(b) ", "(c) ", "(d) "] :
+        ["(a$iter) ", "(b$iter) ", "(c$iter) ", "(d$iter) "]
 
     p_ = plot_subset(bolfi; mode, title_prefixes=prefixes, new_datum, term_cond, display=false, put_in_scale, noise_vars_true, acquisition=PostVariance(), step, y_set=fill(true, y_dim), title)
-    p = plot(p_; size=(540, 2322))
+    if square_layout
+        p = plot(p_; size=(1080, 1080))
+    else
+        p = plot(p_; size=(540, 2322))
+    end
     return p
 end
 
-function plot_sbfs(bolfi; new_datum=nothing, iter=nothing, term_cond, put_in_scale=false, acquisition, step)
+function plot_sbfs(bolfi; square_layout=false, new_datum=nothing, iter=nothing, term_cond, put_in_scale=false, acquisition, step)
     @assert acquisition isa SetsPostVariance
-    mode = :T2
-    noise_vars_true = ToyProblem.σe_true(Val(mode)) .^ 2
+    mode = square_layout ? :T2Square : :T2Column
+    noise_vars_true = ToyProblem.σe_true() .^ 2
 
-    prefixes = [
-        ["(a) ", "(c) ", "(e) ", "(g) "],
-        ["(b) ", "(d) ", "(f) ", "(h) "],
-    ]
+    prefixes = square_layout ?
+        [
+            ["(a) ", "(b) ", "(c) ", "(d) "],
+            ["(e) ", "(f) ", "(g) ", "(h) "],
+        ] :
+        [
+            ["(a) ", "(c) ", "(e) ", "(g) "],
+            ["(b) ", "(d) ", "(f) ", "(h) "],
+        ]
     acq_prefix = "(i) "
 
     subset_plots = [plot_subset(get_subset(bolfi, bolfi.y_sets[:,i]); mode, title_prefixes=prefixes[i], new_datum, term_cond, display=false, put_in_scale, noise_vars_true=noise_vars_true[bolfi.y_sets[:,i]], acquisition=PostVariance(), step, y_set=bolfi.y_sets[:,i], title=set_titles[i]) for i in 1:size(bolfi.y_sets)[2]]
     acq_plot = plot_acquisition(bolfi; mode, title_prefix=acq_prefix, new_datum, acquisition, step)
     
-    p_subsets = plot(subset_plots...; layout=grid(1, length(subset_plots)))
-    s = plot(; framestyle=:none, bottom_margin=-100Plots.px)
-    p = plot(p_subsets, s, acq_plot; layout=grid(3, 1; heights=[0.79, 0.02, 0.19]), size=(1080, 2916))
-    return p
+    if square_layout
+        p_subsets = plot(subset_plots...; layout=grid(1, length(subset_plots)))
+        s = plot(; framestyle=:none, bottom_margin=-100Plots.px)
+        p = plot(p_subsets, s, acq_plot; layout=grid(3, 1; heights=[0.67, 0.03, 0.30]), size=(2160, 1782))
+        return p
+    else
+        p_subsets = plot(subset_plots...; layout=grid(1, length(subset_plots)))
+        s = plot(; framestyle=:none, bottom_margin=-100Plots.px)
+        p = plot(p_subsets, s, acq_plot; layout=grid(3, 1; heights=[0.79, 0.02, 0.19]), size=(1080, 2916))
+        return p
+    end
 end
 
-function plot_true_posteriors(; save=false)
-    mode = :T2
-    y_sets = ToyProblem.get_y_sets(Val(mode))
+function plot_true_posteriors(; save=false, ftype="png")
+    y_sets = ToyProblem.get_y_sets()
     x_prior = ToyProblem.get_x_prior()
-    noise_vars_true = ToyProblem.σe_true(Val(mode)).^2
+    noise_vars_true = ToyProblem.σe_true().^2
 
     step = 0.05 # TODO
 
@@ -193,19 +226,19 @@ function plot_true_posteriors(; save=false)
     @warn "Hard-coded `q ← $q`!"
 
     function ll_post(x, y_set)
-        y = ToyProblem.experiment(Val(mode), x; noise_vars=zeros(ToyProblem.y_dim(Val(mode))))[y_set]
+        y = ToyProblem.experiment(x; noise_vars=zeros(ToyProblem.y_dim()))[y_set]
         
         # ps = numerical_issues(x) ? 0. : 1.
         isnothing(y) && return 0.
 
-        ll = pdf(MvNormal(y, sqrt.(noise_vars_true[y_set])), ToyProblem.y_obs(Val(mode))[y_set])
+        ll = pdf(MvNormal(y, sqrt.(noise_vars_true[y_set])), ToyProblem.y_obs()[y_set])
         pθ = pdf(x_prior, x)
         return pθ * ll
     end
 
     ps = [plot_post((x)->ll_post(x, y_set), x_prior, step, xs, q) for y_set in eachcol(y_sets)]
     display.(ps)
-    save && (savefig.(ps, "true_post_" .* ["1", "2"] .* ".pdf"))
+    save && (savefig.(ps, "true_post_" .* ["1", "2"] .* '.' .* ftype))
     return ps
 end
 
@@ -217,7 +250,7 @@ function plot_post(ll, x_prior, step, xs, q)
     conf_sets = [(p=post_real, c=c_real, label=nothing, color=conf_reg_real)]
 
     p1 = plot(; size=(621,621), colorbar=false)
-    plot_posterior!(p1, (a,b)->ll([a,b]); mode=:TruePost, conf_sets, lims, label=nothing, step)
+    plot_posterior!(p1, (a,b)->ll([a,b]); mode=:SinglePlot, conf_sets, lims, label=nothing, step)
     return p1
 end
 
@@ -240,7 +273,11 @@ function plot_acquisition(bolfi; mode, title_prefix="", new_datum, acquisition, 
     annotate!(p4, -5 - LO_[2,1], +5 + LO_[2,2], text("θ₂", :left, text_size; color=:black))
     
     s = plot(; framestyle=:none, bottom_margin=-100Plots.px)
-    return plot(s, p4, s; layout=grid(1, 3; widths=[0.23, 0.54, 0.23]))
+    if mode == :T2Square
+        return plot(s, p4, s; layout=grid(1, 3; widths=[0.375, 0.25, 0.375]))
+    else
+        return plot(s, p4, s; layout=grid(1, 3; widths=[0.23, 0.54, 0.23]))
+    end
 end
 
 function plot_subset(bolfi; mode, title_prefixes=fill("", 4), new_datum=nothing, term_cond, display=true, put_in_scale=false, noise_vars_true, acquisition, step=0.05, y_set=fill(true, ToyProblem.y_dim), title=nothing)
@@ -256,12 +293,12 @@ function plot_subset(bolfi; mode, title_prefixes=fill("", 4), new_datum=nothing,
 
     # unnormalized posterior likelihood `p(y | x) * p(x) ∝ p(x | y)`
     function ll_post(x)
-        y = ToyProblem.experiment(Val(mode), x; noise_vars=zeros(ToyProblem.y_dim(Val(mode))))[y_set]
+        y = ToyProblem.experiment(x; noise_vars=zeros(ToyProblem.y_dim()))[y_set]
         
         # ps = numerical_issues(x) ? 0. : 1.
         isnothing(y) && return 0.
 
-        ll = pdf(MvNormal(y, sqrt.(noise_vars_true)), ToyProblem.y_obs(Val(mode))[y_set])
+        ll = pdf(MvNormal(y, sqrt.(noise_vars_true)), ToyProblem.y_obs()[y_set])
         pθ = pdf(x_prior, x)
         return pθ * ll
     end
@@ -301,7 +338,7 @@ function plot_subset(bolfi; mode, title_prefixes=fill("", 4), new_datum=nothing,
 
     conf_sets_real = [
         (p=post_real, c=c_real, label="true posterior", color=conf_reg_real),
-        (p=post_expect, c=c_expect, label="expect. posterior", color=conf_reg_expect),
+        TRUE_POST ? fill(nothing, 3) : (p=post_expect, c=c_expect, label="expect. posterior", color=conf_reg_expect),
         (p=post_approx, c=c_approx, label="approx. posterior", color=conf_reg_approx),
     ]
     conf_sets_approx = [
@@ -311,14 +348,17 @@ function plot_subset(bolfi; mode, title_prefixes=fill("", 4), new_datum=nothing,
     ]
 
     # calculate IoU ratios
+    in_real = (post_real.(eachcol(xs)) .> c_real)
     in_approx = (post_approx.(eachcol(xs)) .> c_approx)
     in_expect = (post_expect.(eachcol(xs)) .> c_expect)
     in_lb = (post_lb.(eachcol(xs)) .> c_lb)
     in_ub = (post_ub.(eachcol(xs)) .> c_ub)
+    ar_ratio = set_iou(in_approx, in_real, x_prior, xs)
     ae_ratio = set_iou(in_approx, in_expect, x_prior, xs)
     ublb_ratio = set_iou(in_ub, in_lb, x_prior, xs)
     
     # gp-approximated posterior likelihood
+    ll_real(a, b) = post_real([a, b])
     ll_expect(a, b) = post_expect([a, b])
     ll_approx(a, b) = post_approx([a, b])
 
@@ -333,10 +373,16 @@ function plot_subset(bolfi; mode, title_prefixes=fill("", 4), new_datum=nothing,
     end
     kwargs = (colorbar=false,)
 
-    p1 = plot(; title=title_prefixes[1]*"Expected Posterior", clims, kwargs...)
-    plot_posterior!(p1, ll_expect; mode, conf_sets=conf_sets_real, lims, label=nothing, step)
+    t_ = TRUE_POST ? "True Posterior" : "Expected Posterior"
+    ll_ = TRUE_POST ? ll_real : ll_expect
+    p1 = plot(; title=title_prefixes[1]*t_, clims, kwargs...)
+    plot_posterior!(p1, ll_; mode, conf_sets=conf_sets_real, lims, label=nothing, step)
     plot_samples!(p1, X; mode, new_datum, label=nothing)
-    annotate_iou!(p1, "A-E IoU: $(@sprintf("%.4f", ae_ratio))")
+    if TRUE_POST
+        annotate_iou!(p1, "A-T IoU: $(@sprintf("%.4f", ar_ratio))")
+    else
+        annotate_iou!(p1, "A-E IoU: $(@sprintf("%.4f", ae_ratio))")
+    end
 
     p2 = plot(; title=title_prefixes[2]*"Approx. Posterior", clims, kwargs...)
     plot_posterior!(p2, ll_approx; mode, conf_sets=conf_sets_approx, lims, label=nothing, step)
@@ -352,8 +398,16 @@ function plot_subset(bolfi; mode, title_prefixes=fill("", 4), new_datum=nothing,
     plot_samples!(p4, X; mode, new_datum, label=nothing)
 
     isnothing(title) && (title = put_in_scale ? "(in scale)" : "(not in scale)")
-    t = plot(; title, framestyle=:none, bottom_margin=-160Plots.px, bigtitle_kwargs...)
-    p = plot(t, p1, p2, p3, p4; layout=grid(1 + 4, 1; heights=[0.05, fill(0.95/4, 4)...]))
+    if mode == :T1Square
+        p = plot(p1, p2, p3, p4; layout=grid(2, 2))
+    elseif mode == :T2Square
+        t = plot(; title, framestyle=:none, bottom_margin=-100Plots.px, bigtitle_kwargs...)
+        p_ = plot(p1, p2, p3, p4; layout=grid(2, 2))
+        p = plot(t, p_; layout=grid(2, 1; heights=[0.05, 0.95]))
+    else
+        t = plot(; title, framestyle=:none, bottom_margin=-160Plots.px, bigtitle_kwargs...)
+        p = plot(t, p1, p2, p3, p4; layout=grid(1 + 4, 1; heights=[0.05, fill(0.95/4, 4)...]))
+    end
     display && Plots.display(p)
     return p
 end
@@ -364,12 +418,19 @@ function plot_posterior!(p, ll; mode, conf_sets=[], lims, label=nothing, step=0.
     ticks = -4:2:4
 
     # margins & axis ticks
-    ytick_offset(::Val{:T2}) = 8
-    ytick_offset(::Val{:T1}) = 5
-    ytick_offset(::Val{:TruePost}) = 0
+    ytick_offset(::Val{:T2Column}) = 8
+    ytick_offset(::Val{:T2Square}) = 6
+    ytick_offset(::Val{:T1Column}) = 5
+    ytick_offset(::Val{:T1Square}) = 2
+    ytick_offset(::Val{:SinglePlot}) = 0
+
+    left_margin(::Val) = -5Plots.px
+    left_margin(::Val{:T1Column}) = 0Plots.px
+    left_margin(::Val{:T1Square}) = -20Plots.px
+    left_margin(::Val{:T2Square}) = -60Plots.px
 
     other = (
-        left_margin = (mode == :T1) ? 0Plots.px : -5Plots.px,
+        left_margin = left_margin(Val(mode)),
         right_margin = 5Plots.px,
         top_margin = -10Plots.px,
         bottom_margin = -20Plots.px,
@@ -377,8 +438,8 @@ function plot_posterior!(p, ll; mode, conf_sets=[], lims, label=nothing, step=0.
         aspect_ratio = :equal,
         widen = 1.02,
 
-        xtickfontvalign = (mode == :TruePost) ? :center : :bottom,
-        ytickfonthalign = (mode == :TruePost) ? :center : :left,
+        xtickfontvalign = (mode == :SinglePlot) ? :center : :bottom,
+        ytickfonthalign = (mode == :SinglePlot) ? :center : :left,
         yticks = (ticks, " "^ytick_offset(Val(mode)) .* string.(ticks)),
         xticks = (ticks, string.(ticks)),
 
@@ -405,11 +466,12 @@ function plot_posterior!(p, ll; mode, conf_sets=[], lims, label=nothing, step=0.
 
     # "OBSERVATION-RULES"
     plot!(p, a->1/a, grid; y_lims=lims, label, rule_kwargs...)
-    plot!(p, a->a, grid; y_lims=lims, label, rule_kwargs...)
+    # plot!(p, a->a, grid; y_lims=lims, label, rule_kwargs...)  # TODO
 
     # CONFIDENCE SET
     target = mean(vals)
     for (f, c, kwargs...) in conf_sets
+        isnothing(f) && continue
         plot_confidence_set!(p, f, c; mode, target, lims, step, conf_reg_kwargs..., kwargs...)
     end
 
