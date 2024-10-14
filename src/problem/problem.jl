8b6ed49d1fa1ba2669bc762cc0b08e77b7c109d3
soldasim <simonsoldat@seznam.cz>
2024-10-14 15:16:41 +0200

    reorganize src

--- src/problem/problem.jl
+++ src/problem/problem.jl
@@ -77,3 +77,6 @@ function BolfiProblem(data;
         y_sets,
     )
 end
+
+x_dim(bolfi::BolfiProblem) = BOSS.x_dim(bolfi.problem)
+y_dim(bolfi::BolfiProblem) = BOSS.y_dim(bolfi.problem)
--- src/utils.jl
+++
@@ -1,3 +0,0 @@
-
-x_dim(bolfi::BolfiProblem) = BOSS.x_dim(bolfi.problem)
-y_dim(bolfi::BolfiProblem) = BOSS.y_dim(bolfi.problem)
