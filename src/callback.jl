be30b76649c320657ccc34b72d7ccf55900df128
Sheld5 <simonsoldat@seznam.cz>
2024-11-25 12:47:08 +0100

    add 'CombinedCallback'

---
+++ src/callback.jl
@@ -0,0 +1,44 @@
+
+"""
+If a callback `cb` of type `BolfiCallback` is defined in `BolfiOptions`,
+the method `cb(::BolfiProblem; kwargs...)` will be called in every iteration.
+
+```
+cb(problem::BolfiProblem;
+    model_fitter::BOSS.ModelFitter,
+    acq_maximizer::BOSS.AcquisitionMaximizer,
+    acquisition::AcqWrapper,                # `BolfiAcquisition` wrapped into `AcqWrapper`
+    term_cond::TermCond,                    # either `BOSS.TermCond` or a `BolfiTermCond` wrapped into `TermCondWrapper`
+    options::BossOptions,
+    first::Bool,
+)
+```
+"""
+abstract type BolfiCallback end
+
+"""
+Combines multiple `BolfiCallback`s.
+"""
+struct CombinedCallback <: BolfiCallback
+    callbacks::Vector{BolfiCallback}
+end
+CombinedCallback(cbs...) = CombinedCallback([cbs...])
+
+function (comb::CombinedCallback)(bolfi::BolfiProblem; kwargs...)
+    for cb in comb.callbacks
+        cb(bolfi; kwargs...)
+    end
+end
+
+"""
+Wrapper for BOSS around `BolfiCallback`.
+"""
+struct CallbackWrapper{
+    CB<:BolfiCallback
+} <: BossCallback
+    callback::CB
+    bolfi::BolfiProblem
+end
+CallbackWrapper(cb::BossCallback, ::BolfiProblem) = cb
+
+(wrap::CallbackWrapper)(::BossProblem; kwargs...) = wrap.callback(wrap.bolfi; kwargs...)
