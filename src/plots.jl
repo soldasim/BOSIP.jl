020d5ce0a7e6bb268ae6bcc39361410eb6cbccfe
soldasim <simonsoldat@seznam.cz>
2025-01-13 20:01:22 +0100

    improve marginal plots

--- src/plots.jl
+++ src/plots.jl
@@ -8,18 +8,23 @@ Aggregates all plot settings for the `plot_marginals_int` and `plot_marginals_kd
 - `plot_step::Float64`: Controls the plot resolution. Greatly impacts the computational cost of plotting.
 - `param_labels::Union{Nothing, Vector{String}}`: Labels used for the parameters in the plots.
         Defaults to `nothing`, in which case the default labels "x1,x2,..." are used.
+- `plot_samples::Bool`: Only relevant to `plot_marginals_kde`. Controls whether the samples
+        from the posterior are also plotted.
+- `full_matrix::Bool`: Set to `full_matrix=false` to only plot the marginals below the diagonal
+        and skipped the redundant mirrored plots above the diagonal.
 """
 abstract type PlotSettings end
 
 """
     using CairoMakie
-    plot_marginals_int(::BolfiProblem)
+    plot_marginals_int(::BolfiProblem; kwargs...)
 
 Create a matrix of plots displaying the marginal posterior distribution of each pair of parameters
 with the individual marginals of each parameter on the diagonal.
 
 Approximates the marginals by numerically integrating the marginal integrals
 over a generated latin hypercube grid of parameter samples.
+The plots are normalized according to the plotting grid.
 
 # Kwargs
 
@@ -33,23 +38,26 @@ function plot_marginals_int end
 
 """
     using CairoMakie, Turing
-    plot_marginals_kde(::BolfiProblem)
+    plot_marginals_kde(::BolfiProblem; kwargs...)
 
 Create a matrix of plots displaying the marginal posterior distribution of each pair of parameters
 with the individual marginals of each parameter on the diagonal.
 
 Approximates the marginals by kernel density estimation
 over parameter samples drawn by MCMC methods from the Turing.jl package.
+The plots are normalized according to the plotting grid.
 
-One should experiment with different kernel length scales to obtain a good approximation
-of the marginals. In case of a `Kernel` from `KernelFunctions`, the length scale can be changed
-via the `with_lengthscale` function. (E.g. `with_lengthscale(GaussianKernel(), 0.1)`)
+One should experiment with different kernel length-scales to obtain a good approximation
+of the marginals. The kernel and length-scales are provided via the `kernel` and `lengthscale`
+keyword arguments.
 
 # Kwargs
 
 - `turing_options::TuringOptions`: Settings for the MCMC sampling.
-- `kernel::Kernel`: The kernel used in the KDE. Either as an instance of `KernelFunctions.Kernel`
-        or any function with signature `(x::AbstractVector{<:Real}, x_::AbstractVector{<:Real}) -> ::Real`.
+- `kernel::Kernel`: The kernel used in the KDE.
+- `lengthscale::Union{<:Real, <:AbstractVector{<:Real}}`: The lengthscale for the kernel used in the KDE.
+        Either provide a single length-scale used for all parameter dimensions as a real number,
+        or provide individual length-scales for each parameter dimension as a vector of real numbers.
 - `plot_settings::PlotSettings`: Settings for the plotting.
 - `info::Bool`: Set to `false` to disable prints.
 - `display::Bool`: Set to `false` to not display the figure. It is still returned.
