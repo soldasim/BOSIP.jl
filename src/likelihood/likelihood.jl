00336241a399c9a1817bc25e361c83c67c34a14a
soldasim <simonsoldat@seznam.cz>
2025-02-04 14:48:22 +0100

    add option to use simulator noise as 'obs_std' in 'GaussianLikelihood'

--- src/likelihood/likelihood.jl
+++ src/likelihood/likelihood.jl
@@ -6,32 +6,32 @@ Represents the assumed likelihood of the experiment observation ``y_o``.
 
 To define a custom likelihood, create a new struct `CustomLike <: Likelihood` and implement the following methods;
 
-- `approx_likelihood(::CustomLike, gp_post)`: Necessary to be able to use `approx_posterior`.
-- `likelihood_mean(::CustomLike, gp_post)`: Necessary to be able to use `posterior_mean`.
-- `sq_likelihood_mean(::CustomLike, gp_post)`: Necessary to be able to use `posterior_variance`.
+- `approx_likelihood(::CustomLike, bolfi, gp_post)`: Necessary to be able to use `approx_posterior`.
+- `likelihood_mean(::CustomLike, bolfi, gp_post)`: Necessary to be able to use `posterior_mean`.
+- `sq_likelihood_mean(::CustomLike, bolfi, gp_post)`: Necessary to be able to use `posterior_variance`.
 - `get_subset(::CustomLike, y_set::AsbtractVector{<:Bool})`: Necessary for `BolfiProblem`s where `!isnothing(problem.y_sets)`.
 """
 abstract type Likelihood end
 
 """
-    approx_likelihood(::Likelihood, gp_post)
+    approx_likelihood(::Likelihood, bolfi, gp_post)
 
 Returns a function mapping ``x`` to ``\\hat{p}(y_o|x)``.
 """
-function approx_likelihood(::Likelihood, gp_post) end
+function approx_likelihood(::Likelihood, bolfi, gp_post) end
 
 """
-    likelihood_mean(::Likelihood, gp_post)
+    likelihood_mean(::Likelihood, bolfi, gp_post)
 
 Returns a function mapping ``x`` to ``\\mathbb{E}[ \\hat{p}(y_o|x) | GP ]``.
 """
-function likelihood_mean(::Likelihood, gp_post) end
+function likelihood_mean(::Likelihood, bolfi, gp_post) end
 
 """
-    sq_likelihood_mean(::Likelihood, gp_post)
+    sq_likelihood_mean(::Likelihood, bolfi, gp_post)
 
 Returns a function mapping ``x`` to ``\\mathbb{E}[ \\hat{p}(y_o|x)^2 | GP ]``.
 """
-function sq_likelihood_mean(::Likelihood, gp_post) end
+function sq_likelihood_mean(::Likelihood, bolfi, gp_post) end
 
 function get_subset(::Likelihood, y_set::AbstractVector{<:Bool}) end
