23ffcfed25380a8e3ef6cfd626a835c3c1749349
soldasim <simonsoldat@seznam.cz>
2025-02-20 14:07:07 +0100

    export 'x_dim', 'y_dim'

--- src/BOLFI.jl
+++ src/BOLFI.jl
@@ -3,6 +3,7 @@ module BOLFI
 export bolfi!
 export estimate_parameters!, maximize_acquisition, eval_objective!
 export BolfiProblem
+export x_dim, y_dim
 
 export approx_posterior, posterior_mean, posterior_variance
 export approx_likelihood, likelihood_mean, likelihood_variance
