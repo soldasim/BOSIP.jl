3ed2a143e620dc7096612b7d2dd105eb5c5354a2
soldasim <soldasim@fel.cvut.cz>
2026-02-02 16:36:13 +0100

    fix batching for posterior variance

--- src/posterior/likelihood.jl
+++ src/posterior/likelihood.jl
@@ -17,8 +17,10 @@ function log_likelihood_variance(like::Likelihood, model_post::ModelPosterior)
     log_like_mean = log_likelihood_mean(like, model_post)
     log_sq_like_mean = log_sq_likelihood_mean(like, model_post)
 
-    function log_like_var(x::AbstractVector{<:Real})
+    function log_like_var(x::AbstractArray{<:Real})
         # return sq_like_mean(x) - like_mean(x)^2
-        return log( exp(log_sq_like_mean(x)) - exp(2 * log_like_mean(x)) )
+        log_sqL = log_sq_like_mean(x)
+        log_L = log_like_mean(x)
+        return @. log( exp(log_sqL) - exp(2 * log_L) )
     end
 end
